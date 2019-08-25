5978814fd8edc6df0dad9fcf8a03519cdcdeee2e
KWJr <kwilliam08@gmail.com>
2019-08-25 19:44:19 -0400

    footer background

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -40,24 +40,34 @@ function checkForMatch(cardImageElement) {
 	  console.log("Sorry, try again.");
 	};
 };
-/*created a function for indicating what to log to the console when two cards match.*/
-/*I'm trying to use a method to add the front images of each card to the src element*/
+/*Using the this.setAttribute method left cardId undefined and caused a break in the code.*/
+/*To resolve this issue, I defined the variable cardId in this function.*/
+/*It involved creating a new object for it that would contain the card image ID.*/
+/*That object also received the src attribute of the card images along with the actual image of the card*/
+/*The this.setAttribut method was replaced by the cardImageElement.setAttribute method.*/
+/*Plugged in the object as an arguement in the checkForMatch function.*/
+/*Invoking the function now allows me to see the face of the card image.*/
+
+
 function flipCard(event) {
 	var imageElement = event.target;
 	var cardId = imageElement.getAttribute('data-id');
-
+	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cardsInPlay);
-
 	checkForMatch(imageElement);
-
-
-	// console.log(cards[cardId].cardImage);
-	// console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
-/*created a flipcard function that logs to the console the ID of the card selected with the image and suit.*/
-/*Each time a card is selected another card is added to the empty array.*/
-/*Trouble with calling the checkForMatch function within this function.*/
+/*Again, adding "this" to a method (in this case, getAttribute) wasn't working.*/
+/*cardId and "this" appeared as undefined .*/
+/*I learned about the event.target method that allows me to reference the image that was clicked on.*/
+/*That method is contained in the variable imageElement, which replaces "this".*/
+/*When the flipcard function is called, it provides the face value of the card.*/
+/*Plus, it consoles "User flipped rank of card.*/
+/*The function also pushes the card rank to the empty array*/
+/*Additionally, checkForMatch function provides the image of the card selected.*/
+/*Lastly, the card image and suit are consoled. */
 
 function createBoard() {
 	var board = document.getElementById('game-board');
@@ -71,6 +81,19 @@ function createBoard() {
 		board.appendChild(cardElement);
 	}
 };
+/*This function set the stage for the rest of the functions in this code.*/
+/*I created a variable called, "board" that contains a reference the ID of the div tag with the images of the cards*/
+/*The loop pertains to how a user interacts with the cards on the board*/
+/*Var "i" will move up the index of the "cards" array by an increment of 1 until its value is 4.*/
+/*The code to execute within this statement is focused on seeing a clicking the cards on the board*/
+/*The variable "cardElement" contains an element that refers to the img tag in the html document.*/
+/*The variable receives all the information attached to the images of the each card.*/
+/*Other than the img tag that includes the src, image link, data-id and reference to placement of cards in index.*/
+/*The "cardElement" received an event listener that would invoke the flipcard function each time a card was clicked.*/
+/*Lastly, the appendChild method adds a card to the div tag hold the face value images of the cards.*/
+
+
+
 /*this function involves a loop that should execute the block 4 times.*/
 /*during the loop, the image of the selected card should be revealed when it's clicked.*/
 
